dafc4b6ee6d5dc205b1854b48eb09b4609efa02c
koji-n <koji-n@basil.ocn.ne.jp>
2024-01-24 23:43:19 +0900

    GitHub Actions

--- provision.ps1
+++ provision.ps1
@@ -6,14 +6,19 @@ param (
     [string]$functionName
 )
 
+# (Add)
+az account set -s 16d58f7f-b230-45a2-831e-954f9e3e4ce9
+
 # Creates a Resource group to hold our application
-az group create -l eastus -n $resourceGroup
+#az group create -l japaneast -n $resourceGroup
 
 # Storage account where Azure Functions will keep its data and logs
-az storage account create --sku Standard_LRS --location EastUS --kind Storage -g $resourceGroup -n $storageName.ToLower()
+#az storage account create --sku Standard_LRS --location japaneast --kind Storage -g $resourceGroup -n $storageName.ToLower()
 
 # Creates our Function Application
-az functionapp create -n $functionName -g $resourceGroup -s $storageName.ToLower() -c eastus
+az functionapp create -n $functionName -g $resourceGroup -s $storageName.ToLower() -c japaneast --functions-version 4
 
 # Configures Functions Application Settings
-az functionapp config appsettings set -g $resourceGroup -n $functionName --settings GitHubToken=$githubToken
+#az functionapp config appsettings set -g $resourceGroup -n $functionName --settings GitHubToken=$githubToken
+
+#.\provision.ps1 -githubToken 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 -resourceGroup n1948resourcegroup0124 -storageName n1948strage0124 -functionName n1948func0124
